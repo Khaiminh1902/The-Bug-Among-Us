43d023d870401b2ff648afbda7f014e2617421bc
Khaiminh1902 <khaiminhnguyen1902@gmail.com>
2026-04-07 17:19:57 +0700

    fixes to deploy:)

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import * as Y from "yjs";
 import { codeTemplates } from "./data/challenges/index.ts";
 const PORT = process.env.PORT || 3000;
 
-const dev = true;
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const docs: { [roomId: string]: Y.Doc } = {};
